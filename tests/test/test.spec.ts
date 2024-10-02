f059692eafefedc3b442665c0bc714ceb8a92ba5
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-10-02 15:29:35 -0400

    deleting incorrect test

--- tests/test/test.spec.ts
+++ tests/test/test.spec.ts
@@ -4,8 +4,3 @@ test('test title correct', async ({ page }) => {
   await page.goto("/");
   expect(await page.title()).toBe('Hexo');
 });
-
-test('test title incorrect', async ({ page }) => {
-  await page.goto("/");
-  expect(await page.title()).toBe('Random');
-});
