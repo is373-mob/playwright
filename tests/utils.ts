8d3e932c374e067eee93e97850d138bf2135a79f
Michael <94207078+michaelpeluso@users.noreply.github.com>
2024-10-18 02:35:55 -0400

    Update utils.ts

--- tests/utils.ts
+++ tests/utils.ts
@@ -49,6 +49,7 @@ let MENU_LINKS: string[];
         console.warn("Could not load base URL, menu links, or content paths from _config.yml, falling back to default values");
         BASE_URL = "http://localhost:4000";
         NAVIGATION_PATHS = ["/"];
+        MENU_LINKS = ["/"];
     }
 })();
 
